8ac2997c0c05793ef33ae8eda0ec50f86eb4a592
Mrrrrr10 <104758993@qq.com>
2021-05-02 16:40:07 +0800

    feat：shaded-configs方式

---
+++ nacos-config-depth/order-service/src/main/java/com/nolookblog/order/controller/OrderController.java
@@ -0,0 +1,37 @@
+package com.nolookblog.order.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author Mrrrrr10
+ * @github https://github.com/Mrrrrr10
+ * @blog https://nolookblog.com/
+ * @description
+ */
+
+@RestController
+@RequestMapping("/order")
+@RefreshScope
+public class OrderController {
+
+	@Value("${redis.url}")
+	private String redisUrl;
+
+	@Value("${mybatis-plus.mapper-locations}")
+	private String locations;
+
+	@GetMapping("/mybatis-plus")
+	public String mybatisplus() {
+		return locations;
+	}
+
+	@GetMapping("/redis")
+	public String redis() {
+		return redisUrl;
+	}
+
+}
